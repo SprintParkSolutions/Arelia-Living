0e0bb1f491b42ee61b0c2ce1bfaf2eee6813e95a
Bhavana16010 <bhavanae@sprintpark.com>
2025-12-17 13:08:34 +0530

    Arelia Website Components Initial Commit

--- force-app/main/default/lwc/idcAboutUs/idcAboutUs.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-import ABOUT_IMAGE from '@salesforce/resourceUrl/IDCaboutUsImage';
-
-export default class IdcAboutUs extends LightningElement {
-     aboutImage = ABOUT_IMAGE;
-    navigateAboutUs() {
-        console.log('Navigate to full About Us page');
-    }
-}
